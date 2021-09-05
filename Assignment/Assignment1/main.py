f111db9bf8ead1e890e7bd259b13239ec101b082
Eiskristallen <qhuangb47@gmail.com>
2021-09-05 12:03:12 -0500

    feat:start of Assignment1

---
+++ Assignment/Assignment1/main.py
@@ -0,0 +1,5 @@
+# import necesarry modules from libary
+import json
+import urllib.request
+import turtle
+import time
--- TestCode/Test.py
+++ TestCode/Test.py
@@ -1,1 +1,20 @@
-print('hello world')
+# print('hello world')
+# for i in range(100):
+#     print(i)
+
+
+# def myClass(first, second):
+#     if first and second:
+#         raise ValueError('hehe')
+#     elif first:
+#         print('aa')
+#     else:
+#         print('vv')
+
+
+# myClass(1, None)
+from datetime import datetime
+from os import getcwd
+odds = [1, 3, 4, 23, 4, 53, 2, 1, 34, 35, 5]
+where_am_I = getcwd()
+print(where_am_I)
