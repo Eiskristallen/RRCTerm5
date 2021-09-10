1ff141f64b4eea66d2fb9cf0a2899658db496cd5
Eiskristallen <qhuangb47@gmail.com>
2021-09-10 14:25:31 -0500

    feat:first time push

---
+++ Assignment1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Good123");
+        }
+    }
+}
